b6803dda09f9c3f12df2a2a08d7afac3c7a022f1
Killereq-PL <86300923+Killereq-PL@users.noreply.github.com>
2025-06-22 18:50:02 +0200

    Update DpadInput.py

--- hackzero/inpututils/DpadInput.py
+++ hackzero/inpututils/DpadInput.py
@@ -1,5 +1,6 @@
 from gpiozero import Button
 import keyboard
+import mouse
 
 # GPIO Pins
 UP_PIN = 0
@@ -7,6 +8,9 @@ DOWN_PIN = 1
 LEFT_PIN = 2
 RIGHT_PIN = 3
 OK_PIN = 4
+MODE_SWITCH_PIN = 5
+
+MODE_SWITCH_TOGGLE = True # True for toggle mode, False for hold (switch) mode
 
 class DpadInput():
     def __init__(self):
@@ -15,6 +19,8 @@ class DpadInput():
         self.left_btn = Button(LEFT_PIN)
         self.right_btn = Button(RIGHT_PIN)
         self.ok_btn = Button(OK_PIN)
+        self.mode_switch_btn = Button(MODE_SWITCH_PIN)
+        self.mode = 0 # 0 for DPad mode, 1 for Mouse mode
         self.button_map = {
             self.up_btn: 'up arrow',
             self.down_btn: 'down arrow',
@@ -22,18 +28,35 @@ class DpadInput():
             self.right_btn: 'right arrow',
             self.ok_btn: 'enter'
         }
-    
-    def press_event(caller: Button):
-        caller_pin = caller.pin
-    
-    def release_event(caller: Button):
-        caller_pin = caller.pin
-
-    def setup_events(self):
-        for pair in self.button_map.items():
-            pair[0].when_activated = self.press_event
-            pair[0].when_deactivated = self.release_event
+        
+        def press_callback(self, map):
+            if self.mode == 0:  # DPad mode
+                keyboard.press(map[1])
+            elif self.mode == 1:  # Mouse mode
+                btn = map[0]
+                if btn == self.up_btn:
+                    mouse.move(0, -10, absolute=False, duration=0.1)
+                elif btn == self.down_btn:
+                    mouse.move(0, 10, absolute=False, duration=0.1)
+                elif btn == self.left_btn:
+                    mouse.move(-10, 0, absolute=False, duration=0.1)
+                elif btn == self.right_btn:
+                    mouse.move(10, 0, absolute=False, duration=0.1)
+                elif btn == self.ok_btn:
+                    mouse.press('left')
+        
+        def release_callback(self, map):
+            if self.mode == 0:  # DPad mode
+                keyboard.release(map[1])
+            elif self.mode == 1:  # Mouse mode
+                btn = map[0]
+                if btn == self.ok_btn:
+                    mouse.release('left')
+        
+        for x in self.button_map.items():
+            x[0].when_pressed = lambda: press_callback(self, x)
+            x[0].when_released = lambda: release_callback(self, x)  
 
 if __name__ == "__main__":
     dpadinput = DpadInput()
-    dpadinput.setup_events()
+    print("DPad Input initialized. Press buttons to simulate input.")
