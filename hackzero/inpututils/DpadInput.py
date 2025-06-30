b0581e2c783ce08e47bc6307b4f6c14f4e955cbb
Killereq-PL <86300923+Killereq-PL@users.noreply.github.com>
2025-06-30 20:33:39 +0200

    Update DpadInput.py

--- hackzero/inpututils/DpadInput.py
+++ hackzero/inpututils/DpadInput.py
@@ -29,8 +29,8 @@ class DpadInput():
             self.ok_btn: 'enter'
         }
         
-        def press_callback(self, map):
-            if self.mode == 0:  # DPad mode
+        def press_callback(self: DpadInput, map):
+            if self.mode == 0:  # Dpad mode
                 keyboard.press(map[1])
             elif self.mode == 1:  # Mouse mode
                 btn = map[0]
@@ -45,18 +45,44 @@ class DpadInput():
                 elif btn == self.ok_btn:
                     mouse.press('left')
         
-        def release_callback(self, map):
-            if self.mode == 0:  # DPad mode
+        def release_callback(self: DpadInput, map):
+            if self.mode == 0:  # Dpad mode
                 keyboard.release(map[1])
             elif self.mode == 1:  # Mouse mode
                 btn = map[0]
                 if btn == self.ok_btn:
                     mouse.release('left')
         
+        def hold_callback(self: DpadInput, map):
+            if self.mode == 1:  # Mouse mode
+                btn = map[0]
+                if btn == self.up_btn:
+                    mouse.move(0, -10, absolute=False, duration=0.1)
+                elif btn == self.down_btn:
+                    mouse.move(0, 10, absolute=False, duration=0.1)
+                elif btn == self.left_btn:
+                    mouse.move(-10, 0, absolute=False, duration=0.1)
+                elif btn == self.right_btn:
+                    mouse.move(10, 0, absolute=False, duration=0.1)
+        
         for x in self.button_map.items():
             x[0].when_pressed = lambda: press_callback(self, x)
-            x[0].when_released = lambda: release_callback(self, x)  
+            x[0].when_released = lambda: release_callback(self, x)
+            if x[0] != self.ok_btn:
+                x[0].when_held = lambda: hold_callback(self, x)
+        
+        def mode_toggle(self: DpadInput):
+            self.mode = 1 if self.mode == 0 else 0
+            for x in self.button_map.values():
+                keyboard.release(x)
+            mouse.release('left')
+        
+        if MODE_SWITCH_TOGGLE:
+            self.mode_switch_btn.when_pressed = lambda: mode_toggle(self)
+        else:
+            self.mode_switch_btn.when_pressed = lambda: mode_toggle(self)
+            self.mode_switch_btn.when_released = lambda: mode_toggle(self)
 
 if __name__ == "__main__":
     dpadinput = DpadInput()
-    print("DPad Input initialized. Press buttons to simulate input.")
+    print("Dpad Input initialized\nPress buttons to simulate input")
